957cd266032dc166ebed3718447280e6de3744d8
sunny <skvarma758495@gmail.com>
2025-01-11 16:46:49 +0530

    ghfhtg

--- index.php
+++ index.php
@@ -79,7 +79,7 @@
 
     <!-- Navbar Start -->
     <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
-        <a href="index.html" class="navbar-brand ms-4 ms-lg-0">
+        <a href="index.php" class="navbar-brand ms-4 ms-lg-0">
             <img class="me-3" src="img/jasma-logo-removebg-preview.png" alt="logo" />
         </a>
         <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
@@ -87,12 +87,12 @@
       </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <div class="navbar-nav ms-auto p-4 p-lg-0">
-                <a href="index.html" class="nav-item nav-link active">Home</a>
+                <a href="index.php" class="nav-item nav-link active">Home</a>
                 <a href="about.html" class="nav-item nav-link">About Us</a>
                 <a href="service.html" class="nav-item nav-link">Services</a>
                 <a href="project.html" class="nav-item nav-link">Projects</a>
             </div>
-            <a href="contact.html" class="btn btn-outline-body py-2 px-4" style="margin-left: 26px">Contact</a
+            <a href="contact.php" class="btn btn-outline-body py-2 px-4" style="margin-left: 26px">Contact</a
         >
       </div>
     </nav>
